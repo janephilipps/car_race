8ac9cfa0e552e69aa7c641603f6e61ca5bfd409b
Jane Philipps <jane.philipps@gmail.com>
2015-04-22 21:27:40 -0700

    Adding Race tests & methods

--- lib/race.rb
+++ lib/race.rb
@@ -1,3 +1,30 @@
 require_relative 'car'
 
 # Add Race class here
+class Race
+
+	attr_reader :car1, :car2
+
+	def initialize()
+		@car1 = Car.new
+		@car1.accelerate(rand(100))
+		@car2 = Car.new
+		@car2.accelerate(rand(100))
+	end
+
+	def winner
+		if @car1.speed > @car2.speed
+			@car1
+		elsif @car2.speed > @car1.speed
+			@car2
+		end
+	end
+
+	def loser
+		if @car1.speed > @car2.speed
+			@car2
+		elsif @car2.speed > @car1.speed
+			@car1
+		end
+	end
+end
--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -1,1 +1,36 @@
 require 'spec_helper'
+
+describe Race do
+	# Instantiate two cars when we initialize a race
+	# it '#initialize' do
+	# 	race = Race.new
+	# 	expect(race.car1.speed + race.car2.speed).to eq 0
+	# end
+
+	# Instantiate two cars and accelerate when we initialize a race
+	it '#initialize' do
+		race = Race.new
+		expect(race.car1.speed).not_to eq 0
+	end
+
+	it '#winner' do
+		race = Race.new
+		expect(race.winner.speed > race.loser.speed).to be_truthy
+	end
+
+	it '#winner' do
+		race = Race.new
+		expect(race.winner).to be_a Car
+	end
+
+	it '#loser' do
+		race = Race.new
+		expect(race.loser.speed < race.winner.speed).to be_truthy
+	end
+
+	it '#loser' do
+		race = Race.new
+		expect(race.loser).to be_a Car
+	end
+
+end
